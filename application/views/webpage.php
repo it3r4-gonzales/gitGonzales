c9322f6a7367c49ee56ce8dfc3fe33e9f84513d9
John <gonzalesjohnromil57@gmail.com>
2019-03-24 14:57:43 +0800

    first commit

---
+++ application/views/webpage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GonzalesWeb</title>
+	<link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/testGit.css">
+</head>
+<body>
+	<h3>Welcome to Gonzales Webpage</h3>
+	<p>this is a test</p>
+</body>
+<script type="text/javascript" href="<?php base_url();?>assets/js/testGit.js"></script>
+</html>
